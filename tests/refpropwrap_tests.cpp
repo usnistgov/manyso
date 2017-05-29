51021800fe98dfae09110de94ac77f06f42cd2ef
Ian Bell <ian.bell@gmail.com>
2017-05-28 18:16:09 -0600

    Remove the need to add const to REFPROP_lib.h

--- tests/refpropwrap_tests.cpp
+++ tests/refpropwrap_tests.cpp
@@ -29,9 +29,9 @@ int main(){
         //wrap.SETPATHdll(path, 255); 
 
         long nc = 1, ierr = 0;
-        char herr[255];
+        char herr[255], hfld[10000] = "CO2.FLD", hmx[255] = "HMX.BNC", href[4] = "DEF";
         
-        wrap.SETUPdll(nc, "CO2.FLD", "HMX.BNC", "DEF", ierr, herr, 10000, 255, 3, 255);
+        wrap.SETUPdll(nc, hfld, hmx, href, ierr, herr, 10000, 255, 3, 255);
         if (ierr == 0){
             double z[] = { 1.0 }, x[] = { 1.0 }, y[] = { 1.0 }, T = 300, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = -1, w = -1;
             wrap.TPFLSHdll(T, p, z, d, dl, dv, x, y, h, s, u, cp, cv, w, q, ierr, herr, 255);
